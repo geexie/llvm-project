332285375759962e56accb3b9cb249995b76bdd3
Doithen <doithen@asus.notebook>
2021-03-06 17:17:54 +0300

    First lab, Pavel Tsyplakov

---
+++ llvm/include/llvm/Transforms/TsyplakovCounter/TsyplakovCounter.h
@@ -0,0 +1,28 @@
+#ifndef LLVM_TRANSFORMS_TSYPLAKOVCOUNTER_TSYPLAKOVCOUNTER_H
+#define LLVM_TRANSFORMS_TSYPLAKOVCOUNTER_TSYPLAKOVCOUNTER_H
+
+#define DEBUG_TYPE "TsyplakovCounter"
+
+#include "llvm/IR/PassManager.h"
+#include "llvm/ADT/Statistic.h"
+#include "llvm/Analysis/LoopInfo.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/BasicBlock.h"
+#include "llvm/IR/Instruction.h"
+
+STATISTIC(funcDefCount, "Number of function definitions");
+STATISTIC(loopsCount, "Number of loops");
+STATISTIC(basicBlocksCount, "Number of basic blocks");
+STATISTIC(mulOperationCount, "Number of mul operations");
+STATISTIC(addOperationCount, "Number of add operations");
+
+namespace llvm {
+
+  class PTsyplakovCount : public PassInfoMixin<PTsyplakovCount> {
+  public:
+    PreservedAnalyses run(Function& F, FunctionAnalysisManager& AM);
+    static bool isRequired() { return true; }
+  };
+} // namespace llvm
+
+#endif //LLVM_TRANSFORMS_TSYPLAKOVCOUNTER_TSYPLAKOVCOUNTER_H
