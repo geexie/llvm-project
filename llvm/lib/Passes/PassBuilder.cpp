4448632837239df11e8a4bf93a1e9e1e92ad2573
Marina Kolpakova <marina.kolpakova@intel.com>
2021-04-05 18:17:45 +0300

    Revert "A Soloviova 18pmi lab1 + lab2"

--- llvm/lib/Passes/PassBuilder.cpp
+++ llvm/lib/Passes/PassBuilder.cpp
@@ -219,8 +219,6 @@
 #include "llvm/Transforms/Utils/InjectTLIMappings.h"
 #include "llvm/Transforms/Utils/InstructionNamer.h"
 #include "llvm/Transforms/Utils/LCSSA.h"
-#include "llvm/Transforms/solovevaPass/solovevaPass.h"
-#include "llvm/Transforms/solovevaPass/solovevaPass2.h"
 #include "llvm/Transforms/Utils/LibCallsShrinkWrap.h"
 #include "llvm/Transforms/Utils/LoopSimplify.h"
 #include "llvm/Transforms/Utils/LoopVersioning.h"
