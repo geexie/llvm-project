6d387d36f52337166b0185736ffacbf333669898
Denis Kabanov <denis.kabanov@intel.com>
2021-09-26 18:51:47 +0300

    DenisKabanov-count pass

---
+++ llvm/lib/Transforms/DenisKabanov/DenisKabanov.cpp
@@ -0,0 +1,14 @@
+#include "llvm/Transforms/DenisKabanov/DenisKabanov.h"
+
+#define DEBUG_TYPE "DenisKabanov-count"
+#include "llvm/ADT/Statistic.h"
+
+using namespace llvm;
+// PreservedAnalyses возвращает данные о том, изменили ли мы что-то в коде или нет 
+// если код не изменён - проход без сайд эффектов (всё, что верно до нашего прохода - верно и после)
+// если что-то изменилось - инвалидируем информацию о коде (последующим проходам придётся перезапускать анализ, так как будет трансформационный проход)
+PreservedAnalyses DenisKabanovPass::run(Function &F,
+                                      FunctionAnalysisManager &AM) {
+  errs() << "functions " << F.getName() << "\n"; // возвращает название функции
+  return PreservedAnalyses::all();
+}
