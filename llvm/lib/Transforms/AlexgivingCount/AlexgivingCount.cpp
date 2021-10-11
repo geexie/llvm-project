d298f4c38560003446c98da5f931d881c8e3c167
Trutnev Aleksei <alexgiving@mail.ru>
2021-10-12 00:09:29 +0300

    Add new custom pass

--- llvm/lib/Transforms/AlexgivingCount/AlexgivingCount.cpp
+++ llvm/lib/Transforms/AlexgivingCount/AlexgivingCount.cpp
@@ -1,11 +1,27 @@
 #include "llvm/Transforms/AlexgivingCount/AlexgivingCount.h"
 
- #define DEBUG_TYPE "AlexgivingCount"
- #include "llvm/ADT/Statistic.h"
+#define DEBUG_TYPE "AlexgivingCount"
+#include "llvm/ADT/Statistic.h"
+#include "llvm/IR/InstIterator.h"
 
  using namespace llvm;
  PreservedAnalyses AlexgivingPass::run(Function &F,
                                        FunctionAnalysisManager &AM) {
-   errs() << "functions " << F.getName() << "\n";
+
+    errs() << "functions " << F.getName() << "\n";
+
+    int BB_count = 0;
+    int IN_count = 0;
+    for (auto BB = F.begin(); BB != F.end(); ++BB) {
+        BB_count++;
+
+        for (auto& IN : instructions(F)){
+            IN_count++;
+        }
+    }
+
+    errs() << "    Number of base blocks " << BB_count << "\n";
+    errs() << "        Number of instructions " << IN_count << "\n";
+
    return PreservedAnalyses::all();
  }
