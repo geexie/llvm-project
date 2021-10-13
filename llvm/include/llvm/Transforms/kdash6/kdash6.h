2d49a955aee258bdc9e07f48c6bd6684e26613da
k-dash6 <dakozlova_1@edu.hse.ru>
2021-10-13 20:33:27 +0300

    k-dash6

---
+++ llvm/include/llvm/Transforms/kdash6/kdash6.h
@@ -0,0 +1,25 @@
+//===-- GeexieCount.h - Example Transformations ------------------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_TRANSFORMS_KDASH6_KDASH6_H
+#define LLVM_TRANSFORMS_KDASH6_KDASH6_H
+
+#include "llvm/IR/PassManager.h"
+
+namespace llvm {
+
+class LPMUpdater;
+
+class kdash6Pass : public PassInfoMixin<kdash6Pass> {
+public:
+  PreservedAnalyses run(Function &F, FunctionAnalysisManager &AM);
+};
+
+} // namespace llvm
+
+#endif // LLVM_TRANSFORMS_KDASH6_KDASH6_H
