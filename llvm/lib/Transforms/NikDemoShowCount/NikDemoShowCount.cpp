ad671efa421acd8ade4e9aeaec5716815cdfcf15
Nikitaa Demashov <nikita.demashov@intel.com>
2021-10-13 21:40:42 +0300

    lab1

--- llvm/lib/Transforms/NikDemoShowCount/NikDemoShowCount.cpp
+++ llvm/lib/Transforms/NikDemoShowCount/NikDemoShowCount.cpp
@@ -2,10 +2,28 @@
 
 #define DEBUG_TYPE "NikDemoShowCount"
 #include "llvm/ADT/Statistic.h"
+#include "llvm/IR/InstIterator.h"
 
 using namespace llvm;
+
+ALWAYS_ENABLED_STATISTIC(F_count, "Number of functions");
+ALWAYS_ENABLED_STATISTIC(OPS_count, "Number of BB ");
+ALWAYS_ENABLED_STATISTIC(BB_count, "Number of OPS ");
+
+
 PreservedAnalyses NikDemoShowPass::run(Function &F,
 FunctionAnalysisManager &AM) {
-  errs() << "functions " << F.getName() << "\n";
+  F_count ++;
+  for (auto& BB : F) {
+    BB_count++;
+    for (auto& IN : instructions(F)){
+      OPS_count++;
+    }
+  }
+
+  errs() << "Number of Func " << F_count << "\n";
+  errs() << " Number of BB " << BB_count << "\n";
+  errs() << " Number of OPS " << OPS_count << "\n";
+
   return PreservedAnalyses::all();
 }
