761b4bed744ccfaa4343f0954111dfee0cac7c08
Valeriy Varlachev <vmvarlachyov@edu.hse.ru>
2021-10-13 17:25:17 +0300

    BB counter, Math instructions counter, Loops counter

---
+++ llvm/lib/Transforms/ValVarlCount/ValVarlCount.cpp
@@ -0,0 +1,51 @@
+//===-- ValVarlCount.cpp - Example Transformations --------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "llvm/Transforms/ValVarlCount/ValVarlCount.h"
+
+#include "llvm/ADT/Statistic.h"
+#include "llvm/IR/InstIterator.h"
+#include "llvm/Analysis/LoopInfo.h"
+
+#define DEBUG_TYPE "valvarl-count"
+
+using namespace llvm;
+
+ALWAYS_ENABLED_STATISTIC(BB_count, "BasicBlocks amount");
+ALWAYS_ENABLED_STATISTIC(OPS_count, "Math operations amount");
+ALWAYS_ENABLED_STATISTIC(loops_count, "Loops amount");
+
+PreservedAnalyses ValVarlCountPass::run(Function &F,
+                                      FunctionAnalysisManager &AM) {
+
+    for (auto BB = F.begin(); BB != F.end(); ++BB) ++BB_count;
+
+    for (const auto& I : instructions(F)) {
+        switch (I.getOpcode()) {
+            case Instruction::Add:
+            case Instruction::FAdd:
+            case Instruction::Sub:
+            case Instruction::FSub:
+            case Instruction::Mul:
+            case Instruction::FMul:
+            case Instruction::FDiv:
+            case Instruction::SDiv:
+            case Instruction::UDiv:
+                ++OPS_count;
+        }
+    }
+
+    auto& LA = AM.getResult<LoopAnalysis>(F);
+    for (auto& L : LA) ++loops_count;
+
+    errs() << "BasicBlocks: " << BB_count << "\n"
+            << "Math instructions: " << OPS_count << "\n" 
+            << "Loops: " << loops_count << "\n";
+    
+    return PreservedAnalyses::all();
+}
