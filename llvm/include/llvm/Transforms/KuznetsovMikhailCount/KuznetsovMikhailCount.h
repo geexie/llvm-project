dd4d452230c49d40db931d757f6ab937dcd115ff
KuznetsovMikhail <KuznetsovMikhail>
2021-10-14 23:01:20 +0300

    Added number of loops and some fixes(gstyle, order of output, set instead vector)

--- llvm/include/llvm/Transforms/KuznetsovMikhailCount/KuznetsovMikhailCount.h
+++ llvm/include/llvm/Transforms/KuznetsovMikhailCount/KuznetsovMikhailCount.h
@@ -2,11 +2,14 @@
 #define LLVM_TRANSFORMS_KUZNETSOVMIKHAILCOUNT_KUZNETSOVMIKHAILCOUNT_H_
 
 #include "llvm/IR/PassManager.h"
+#include "llvm/Analysis/LoopInfo.h"
 
 namespace llvm {
-  class KuznetsovMikhailCountPass : public PassInfoMixin<KuznetsovMikhailCountPass> {
+  class KuznetsovMikhailCountPass :
+    public PassInfoMixin<KuznetsovMikhailCountPass> {
    public:
-      PreservedAnalyses run(Function &F, FunctionAnalysisManager &AM);
+      PreservedAnalyses run(Function &F,
+                            FunctionAnalysisManager &AM);
   };
 } // namespace llvm
 
--- llvm/lib/Transforms/KuznetsovMikhailCount/KuznetsovMikhailCount.cpp
+++ llvm/lib/Transforms/KuznetsovMikhailCount/KuznetsovMikhailCount.cpp
@@ -1,32 +1,37 @@
-#include <vector>
+#include <set>
 #include "llvm/ADT/Statistic.h"
 #include "llvm/IR/InstIterator.h"
 #include "llvm/Transforms/KuznetsovMikhailCount/KuznetsovMikhailCount.h"
 
+
 #define DEBUG_TYPE "KuznetsovMikhailCount"
 
-ALWAYS_ENABLED_STATISTIC(opsNum, "Number of arithmetic OPS");
-ALWAYS_ENABLED_STATISTIC(bbNum, "Number of BB");
 ALWAYS_ENABLED_STATISTIC(funNum, "Number of functions");
+ALWAYS_ENABLED_STATISTIC(lpNum, "Number of loops");
+ALWAYS_ENABLED_STATISTIC(bbNum, "Number of BB");
+ALWAYS_ENABLED_STATISTIC(opsNum, "Number of arithmetic OPS");
 
 llvm::PreservedAnalyses llvm::KuznetsovMikhailCountPass::run(Function &F,
                                        FunctionAnalysisManager &AM) {
-  ++funNum;
-  std::vector <unsigned int> arithmeticInstructions {Instruction::Add, Instruction::FAdd, Instruction::Sub, Instruction::FSub, Instruction::Mul, Instruction::SDiv, Instruction::UDiv, Instruction::FDiv};
+  std::set <unsigned int> artmInstr {Instruction::Add, Instruction::FAdd,
+                                    Instruction::Sub, Instruction::FSub,
+                                    Instruction::Mul, Instruction::SDiv,
+                                    Instruction::UDiv, Instruction::FDiv};
   for (auto & bb : F) {
     ++bbNum;
     for (auto & in : bb) {
       auto code = in.getOpcode();
-      for (auto & arIn : arithmeticInstructions) {
-        if(code == arIn) {
-          ++opsNum;
-          break;
-        }
+      if (artmInstr.count(code)) {
+        ++opsNum;
       }
     }
   }
-  errs() << "Number of arithmetic OPS: " << opsNum << "\n";
-  errs() << "Number of BB: " << bbNum << "\n";
+  ++funNum;
+  auto & lpAn = AM.getResult<LoopAnalysis>(F);
+  lpNum += lpAn.end() - lpAn.begin();
   errs() << "Number of functions: " << funNum << "\n";
+  errs() << "Number of loops: " << lpNum << "\n";
+  errs() << "Number of BB: " << bbNum << "\n";
+  errs() << "Number of arithmetic OPS: " << opsNum << "\n";
   return PreservedAnalyses::all();
 }
